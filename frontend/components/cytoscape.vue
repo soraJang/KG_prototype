8b6a7f6bcb38baecf34f19eb0bcccf9cd1dd6939
soraJang <skysora85@gmail.com>
2024-05-28 16:14:23 +0900

    [FAB2023-753] feat : Clustering 1차 구현
    - 부모노드를 'Category' 처럼 사용 하도록 구현
    - 부모노드의 하위 노드의 drag/drop 처리 구현

--- frontend/components/cytoscape.vue
+++ frontend/components/cytoscape.vue
@@ -11,7 +11,10 @@
       :highlighted-node-id="highlightedNodeId"
       @set-node-view="setNodeView"
     ></Filter>
-    <div id="cy" ref="mmContainer" style="width: 1400px; height: 1000px"></div>
+
+      <div class="canvas" style="width: 1600px; height: 1000px">
+        <div id="cy" ref="mmContainer" style="width: 1400px; height: 1000px"></div>
+      </div>
   </div>
 </template>
 
@@ -63,6 +66,115 @@ interface Filter {
   isUnChecked: boolean;
 }
 
+
+const options = ref({
+    grabbedNode: (node) => {
+        // 노드를 선택할 때 마다, 이벤트를 생성해주고 있기 때문에, 노드 선택시 이전 이벤트를 지우고 시작한다. (이벤트가 복수개로 설정되버림.)
+        node.off("cdndout");
+
+        // node.off("cdndgrab");
+        // node.off("cdndover");
+        // node.off("cdnddrop");
+
+        node.on("cdndout", (event, parentNode, dropSibling) => {
+            const cy = event.cy;
+            const thisNode = event.target;
+
+            /**
+             * dragOut 할때 해야할 일
+             * 1. 선택한 노드가 부모 노드(이하 카테고리)에 포함되어 있는 경우, 해당 카테고리의 edges 정보를 물려받아야 함.
+             * 2. 선택한 노드의 카테고리에 선택한 노드를 제외한 노드가 1개 이하인 경우,
+             *    2-1. 다른 하위 노드가 있는 경우, 해당 노드에게 카테고리의 edges 정보를 물려줘야함.
+             *    2-2. 다른 하위 노드의 parent 정보를 삭제해 줘야함. (하위노드 move())
+             *    2-3. 분류를 삭제해 줘야함.
+             */
+
+                // step1. 부모 노드의 edges 정보를 저장해둔다.
+            let targets = parentNode
+                    .connectedEdges()
+                    .map((e) =>
+                        parentNode.data().id === e.data().source ? e.data().target : ""
+                    );
+
+            // edges 정보를 물려줄 node 정보를 array 에 저장해두는 변수 선언
+            // 선택한 노드는 dragout 하는 노드이기 때문에, children 에 넣어둔다.
+            let children = [thisNode];
+
+            // 현재 노드를 dragout 한 후에 남아있는 노드를 구한다.
+            // 선택한 노드는 이미 부모 노드에서 빠져있다.
+            if (parentNode.children().length < 2) {
+                // 부모노드에 남아있는 자식 노드가 1개 이하인 경우
+                // 부모노드의 존재 의미가 없기 때문에 부모노드를 삭제한다.
+                // 삭제하기 전에 자식노드를 독립
+                children = children.concat(parentNode.children());
+
+                // 부모노드의 자식노드에서 해당 parent 정보를 지워준다.
+                parentNode.children().move({ parent: null });
+            }
+
+            // 부모노드에 자식노드가 없다면, 부모노드를 삭제한다.
+            if (!parentNode.isParent()) {
+                parentNode.remove();
+            }
+
+            // children 변수에는 부모노드의 edges 정보를 물려받아야 하는 노드들이 들어있다. 반영해준다.
+            let newEdges = [];
+            children.forEach((e) => {
+                newEdges = newEdges.concat(
+                    targets.map((t) => {
+                        return {
+                            group: "edges",
+                            data: {
+                                id: `${e.data().id}${t}`,
+                                source: e.data().id,
+                                target: t
+                            }
+                        };
+                    })
+                );
+            });
+            cy.add(newEdges);
+        });
+
+        // 사용하지 않음.
+        // node.on("cdndgrab", (event) => {
+        //   console.log("cdndgrab");
+        // });
+        // node.on("cdndover", (event, dropTarget, dropSibling) => {
+        //   console.log("cdndover");
+        // });
+        // node.on("cdnddrop", (event, dropTarget, dropSibling) => {
+        //   console.log("cdnddrop");
+        // });
+
+        return true;
+    },
+    dropTarget: (dropTarget, grabbedNode) => {
+        // grabbedNode : 내가 선택한 노드
+        // dropTarget : 내가 drop 할수 있는 모든 노드.
+        // ->  분류를 추가하는 기능은 지원하지 않음 === 어떤 노드에도 drop 할 수 없음. 위치 이동만 가능함.
+        return false;
+    },
+    dropSibling: (dropSibling, grabbedNode) => {
+        // grabbedNode : 내가 선택한 노드
+        // dropSibling : 고아노드를 제외한 내가 drop 할 수 있는 노드
+        // dropTarget 은 false 고 dropSibling 은 true 일 수 없음.
+        return false;
+    },
+    newParentNode: (grabbedNode, dropSibling) => {
+        // 고아노드 + 고아노드 를 합칠때, 부모노드가 생성될때 호출 됨.
+        // 노드 + 노드 를 설정할 수 없도록 했기 때문에 이 option method 에서는 아무것도 할 필요가 없음.
+        return {};
+    },
+    boundingBoxOptions: {
+        // same as https://js.cytoscape.org/#eles.boundingBox, used when calculating if one node is dragged over another
+        includeOverlays: false,
+        includeLabels: false
+    },
+    overThreshold: 10, // make dragging over a drop target easier by expanding the hit area by this amount on all sides
+    outThreshold: 10 // make dragging out of a drop target a bit harder by expanding the hit area by this amount on all sides
+});
+
 const mmContainer = ref(null);
 const filters = ref<Filter[]>([]);
 let highlightedNodeId = ref([]);
@@ -78,6 +190,7 @@ onMounted(() => {
     zoom: 1,
     layout: cytoscapeStore.graphLayouts[props.defaultLayout]
   });
+    const cdnd = cy.compoundDragAndDrop(options.value);
 
   cy.elements().forEach((element: any) => {
     let colorCode: string = getRandomColor();

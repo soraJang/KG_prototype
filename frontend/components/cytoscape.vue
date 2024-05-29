332e1e3e562ac6619b5dabb7ce5428e4f37509a4
ohmy04180 <ohmy0418@mobigen.com>
2024-05-29 15:32:47 +0900

    [FAB2023-751] feat: node, edge 기본 기능 추가

--- frontend/components/cytoscape.vue
+++ frontend/components/cytoscape.vue
@@ -1,19 +1,16 @@
 <template>
   <Top v-if="useTop" class="part top"></Top>
-  <Filter v-if="useFilter" class="part filter" :filters="filters"></Filter>
+  <Filter v-if="useFilter" :filters="filters"></Filter>
   <MouseOption class="part">마우스 우클릭</MouseOption>
   <ControlBtn v-if="useControlBtn" class="part setting"
-    >하단 아래 설정 아이콘</ControlBtn
-  >
-
-  <div
-    ref="mmContainer"
-    style="width: 1400px; height: 1000px; border: black; border-inline-width: 3"
-  ></div>
+    >하단 아래 설정 아이콘
+  </ControlBtn>
+  <div id="cy" ref="mmContainer" style="width: 1400px; height: 1000px"></div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
+
 const nuxtApp = useNuxtApp();
 
 import Filter from "./units/filter.vue";
@@ -22,6 +19,7 @@ import MouseOption from "./units/mouse-option.vue";
 import ControlBtn from "./units/control-btn.vue";
 
 const mmContainer = ref(null);
+const filters = ref([]);
 
 const props = defineProps({
   nodeData: {
@@ -51,7 +49,7 @@ const props = defineProps({
 
 onMounted(() => {
   const cytoscape = nuxtApp.$cytoscape;
-  let cy = null;
+  let cy: any = null;
 
   cy = cytoscape({
     container: mmContainer.value,
@@ -62,17 +60,102 @@ onMounted(() => {
       name: "preset"
     }
   });
+
+  cy.elements().forEach((node: any) => {
+    let colorCode: string = getRandomColor();
+    if (node.isNode()) {
+      node.style("background-color", colorCode);
+      filters.value.push({
+        id: node.id(),
+        label: node.data().label,
+        color: colorCode
+      });
+    }
+  });
+  console.log("filters", filters.value);
+
+  cy.on("tap", "node", (event: any) => {
+    let evtTarget = event.target;
+
+    cy.elements().removeClass("notSelected");
+    evtTarget.addClass("highlight");
+
+    cy.elements().forEach((element: any) => {
+      if (element.isNode() && element !== evtTarget) {
+        element.addClass("notSelected");
+      } else if (element.isEdge()) {
+        element.addClass("notSelected");
+      }
+    });
+  });
+
+  cy.on("tap", "edge", (event: any) => {
+    let evtTarget = event.target;
+    let startNode = evtTarget.source();
+    let endNode = evtTarget.target();
+
+    evtTarget.addClass("highlight");
+    startNode.addClass("highlight");
+    endNode.addClass("highlight");
+
+    cy.elements().forEach((element: any) => {
+      if (element.isNode() && element !== startNode && element !== endNode) {
+        element.addClass("notSelected");
+      } else if (element.isEdge() && element !== evtTarget) {
+        element.addClass("notSelected");
+      }
+    });
+  });
+
+  cy.on("tap", (event: any) => {
+    let evtTarget = event.target;
+
+    if (evtTarget === cy) {
+      cy.elements().forEach((element: any) => {
+        if (element.isNode()) {
+          element.removeClass("highlight");
+          element.removeClass("notSelected");
+        } else if (element.isEdge()) {
+          element.removeClass("highlight");
+          element.removeClass("notSelected");
+        }
+      });
+    }
+  });
+
+  cy.on("mouseover", "node", (event: any) => {
+    let evtTarget = event.target;
+    evtTarget.addClass("mouseHover");
+  });
+
+  cy.on("mouseout", "node", (event: any) => {
+    let evtTarget = event.target;
+    evtTarget.removeClass("mouseHover");
+  });
+
+  cy.on("mouseover", "edge", (event: any) => {
+    let evtTarget = event.target;
+    evtTarget.addClass("mouseHover");
+  });
+
+  cy.on("mouseout", "edge", (event: any) => {
+    let evtTarget = event.target;
+    evtTarget.removeClass("mouseHover");
+  });
 });
+
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 </script>
 
 <style lang="scss" scoped>
 .part {
   border: 1px solid red;
-}
-.top {
-}
-.filter {
-}
-.setting {
 }
 </style>

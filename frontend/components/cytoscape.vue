b3d4b366ab2dff77cbc0269581ecd149800c2734
ohmy04180 <ohmy0418@mobigen.com>
2024-05-30 14:51:17 +0900

    [FAB2023-751] feat: filter 기능 구현

--- frontend/components/cytoscape.vue
+++ frontend/components/cytoscape.vue
@@ -1,6 +1,11 @@
 <template>
   <Top v-if="useTop" class="part top"></Top>
-  <Filter v-if="useFilter" :filters="filters"></Filter>
+  <Filter
+    v-if="useFilter"
+    :filters="filters"
+    :highlighted-node-id="highlightedNodeId"
+    @set-node-view="setNodeView"
+  ></Filter>
   <MouseOption class="part">마우스 우클릭</MouseOption>
   <ControlBtn v-if="useControlBtn" class="part setting"
     >하단 아래 설정 아이콘
@@ -9,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, defineProps } from "vue";
 
 const nuxtApp = useNuxtApp();
 
@@ -17,9 +22,6 @@ import Filter from "./units/filter.vue";
 import Top from "./units/top.vue";
 import MouseOption from "./units/mouse-option.vue";
 import ControlBtn from "./units/control-btn.vue";
-
-const mmContainer = ref(null);
-const filters = ref([]);
 
 const props = defineProps({
   nodeData: {
@@ -47,9 +49,20 @@ const props = defineProps({
   }
 });
 
+interface Filter {
+  id: string;
+  label: string;
+  color: string;
+  isUnChecked: boolean;
+}
+
+const mmContainer = ref(null);
+const filters = ref<Filter[]>([]);
+let highlightedNodeId = ref([]);
+let cy: any = null;
+
 onMounted(() => {
   const cytoscape = nuxtApp.$cytoscape;
-  let cy: any = null;
 
   cy = cytoscape({
     container: mmContainer.value,
@@ -61,21 +74,41 @@ onMounted(() => {
     }
   });
 
-  cy.elements().forEach((node: any) => {
+  cy.elements().forEach((element: any) => {
     let colorCode: string = getRandomColor();
-    if (node.isNode()) {
-      node.style("background-color", colorCode);
+    if (element.isNode()) {
+      element.style("background-color", colorCode);
       filters.value.push({
-        id: node.id(),
-        label: node.data().label,
-        color: colorCode
+        id: element.id(),
+        label: element.data().label,
+        color: colorCode,
+        isUnChecked: false
       });
     }
   });
-  console.log("filters", filters.value);
+
+  cy.on("tap", (event: any) => {
+    let evtTarget = event.target;
+
+    highlightedNodeId.value = [];
+
+    if (evtTarget === cy) {
+      cy.elements().forEach((element: any) => {
+        if (element.isNode()) {
+          element.removeClass("highlight");
+          element.removeClass("notSelected");
+        } else if (element.isEdge()) {
+          element.removeClass("highlight");
+          element.removeClass("notSelected");
+        }
+      });
+    }
+  });
 
   cy.on("tap", "node", (event: any) => {
     let evtTarget = event.target;
+
+    highlightedNodeId.value.push(evtTarget.id());
 
     cy.elements().removeClass("notSelected");
     evtTarget.addClass("highlight");
@@ -94,6 +127,9 @@ onMounted(() => {
     let startNode = evtTarget.source();
     let endNode = evtTarget.target();
 
+    highlightedNodeId.value.push(startNode.id(), endNode.id());
+
+    cy.elements().removeClass("notSelected");
     evtTarget.addClass("highlight");
     startNode.addClass("highlight");
     endNode.addClass("highlight");
@@ -105,22 +141,6 @@ onMounted(() => {
         element.addClass("notSelected");
       }
     });
-  });
-
-  cy.on("tap", (event: any) => {
-    let evtTarget = event.target;
-
-    if (evtTarget === cy) {
-      cy.elements().forEach((element: any) => {
-        if (element.isNode()) {
-          element.removeClass("highlight");
-          element.removeClass("notSelected");
-        } else if (element.isEdge()) {
-          element.removeClass("highlight");
-          element.removeClass("notSelected");
-        }
-      });
-    }
   });
 
   cy.on("mouseover", "node", (event: any) => {
@@ -144,17 +164,32 @@ onMounted(() => {
   });
 });
 
-const getRandomColor = () => {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+let getRandomColor = () => {
+  let color_r = Math.floor(Math.random() * 127 + 115).toString(16);
+  let color_g = Math.floor(Math.random() * 127 + 115).toString(16);
+  let color_b = Math.floor(Math.random() * 127 + 115).toString(16);
+  return `#${color_r + color_g + color_b}`;
+};
+
+const setNodeView = (id: string, isUnChecked: boolean) => {
+  cy.elements().forEach((element: any) => {
+    if (element.isNode()) {
+      if (element.id() === id) {
+        isUnChecked ? element.hide() : element.show();
+      }
+    }
+  });
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+ul,
+li {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
 .part {
   border: 1px solid red;
 }

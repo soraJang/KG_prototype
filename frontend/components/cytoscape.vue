48391d95dd63417ea29affe74acec2395ce79678
soraJang <skysora85@gmail.com>
2024-06-04 09:18:33 +0900

    [FAB2023-753] feta : clustering 상위 노드 dblclick 기능 추가 외
    - dbl click 시 상위노드 숨김처리, 하위노드 전개
    - 하위노드 선택시 상위노드 선택 되도록 처리 (하위노드가 상위노드 분류 안에 있을때, 밖에 있을때 둘다)
    - 그 외 오류 수정

--- frontend/components/cytoscape.vue
+++ frontend/components/cytoscape.vue
@@ -24,6 +24,9 @@ import Filter from "@comp/units/filter.vue";
 import SelectLayout from "@comp/units/selectLayout.vue";
 import useCytoscapeDragOpt from "@composables/cytoscape/dragOpt";
 
+const { option, getTarget, createNewEdge, getChild, getSibling, getParent } =
+  useCytoscapeDragOpt();
+
 const nuxtApp = useNuxtApp();
 
 const cytoscapeStore = useCytoscapeStore();
@@ -66,12 +69,25 @@ interface Filter {
 }
 
 const hideNodeList = ref([]);
-const parentColorObj = ref({});
+const filterList = ref([]);
 
 const mmContainer = ref(null);
 const filters = ref<Filter[]>([]);
 let highlightedNodeId = ref([]);
 let cy: any = null;
+
+const setFilters = () => {
+  filterList.value.forEach((el) => {
+    if (!el.isChild()) {
+      filters.value.push({
+        id: el.id(),
+        label: el.data().label,
+        color: el.data("colorCode"),
+        isUnChecked: false
+      });
+    }
+  });
+};
 
 onMounted(() => {
   const cytoscape = nuxtApp.$cytoscape;
@@ -92,7 +108,7 @@ onMounted(() => {
       y: container.offsetHeight / 2
     }
   });
-  const cdnd = cy.compoundDragAndDrop(useCytoscapeDragOpt().option.value);
+  const cdnd = cy.compoundDragAndDrop(option.value);
 
   // 부모 노드 tag 추가
   cy.nodeHtmlLabel([
@@ -116,31 +132,42 @@ onMounted(() => {
     }
   ]);
 
-  cy.elements().forEach((element: any) => {
+  cy.elements().forEach((el: any) => {
     let colorCode: string = getRandomColor();
-    if (element.isChild()) {
-      // 자식노드는 별도 처리한다.
-      return;
-    }
-    if (element.isParent()) {
-      // 자식 노드 색상을 여기서 설정해둔다.
-      element.data("childColor", getRandomColor());
-    }
 
-    if (element.isNode()) {
-      // 자식 노드 일 경우, 색상을 통일시켜줘야함.
-      element.style("background-color", colorCode);
-      // 자식노드는 filter 에 표시하지 않음.
-      if (!element.isChild()) {
-        filters.value.push({
-          id: element.id(),
-          label: element.data().label,
-          color: colorCode,
-          isUnChecked: false
-        });
+    /**
+     * 노드 돌면서 해야하는 일
+     * - 노드일때
+     *    - child 일때
+     *        - pass
+     *    - parent 일때
+     *        - filterList 에 넣기
+     * - 엣지일때
+     *    -- 없음
+     */
+
+    el.style("background-color", colorCode);
+
+    // Node 일때
+    if (el.isNode()) {
+      filterList.value.push(el);
+      el.data("colorCode", colorCode);
+
+      if (el.isChild()) {
+        // do something
+        const parentData = el.parent().data();
+        el.data(CONSTANTS.PRNT_CTGRY_ID, parentData.id);
+        el.data(CONSTANTS.PRNT_CTGRY_NM, parentData.label);
+      } else if (el.isParent()) {
+        // 자식 노드 색상을 여기서 설정해둔다. (부모 노드와 색상 동일하게)
+        // TODO : 혹은 색상 채도를 좀 낮춰서 표시 (코드가 복잡해서 보류)
+        el.data("childColor", colorCode);
       }
+    } else {
+      // edge 일때
     }
   });
+  setFilters();
 
   cy.elements().forEach((e) => {
     // 자식노드일때, 부모노드에 설정해두었던 색상코드를 일괄적용한다.
@@ -169,6 +196,7 @@ onMounted(() => {
 
   cy.on("tap", "node", (event: any) => {
     let evtTarget = event.target;
+    let parentId = evtTarget.data(CONSTANTS.PRNT_CTGRY_ID);
 
     highlightedNodeId.value.push(evtTarget.id());
 
@@ -194,11 +222,12 @@ onMounted(() => {
       }
     });
 
-    if (evtTarget.isChild()) {
+    if (parentId !== undefined) {
       // 자식노드면 부모 노드도 선택한걸로 처리해줘야함.
-      highlightedNodeId.value.push(evtTarget.parent().data().id);
-      evtTarget.parent().removeClass(CONSTANTS.NOT_SELECTED);
-      evtTarget.parent().addClass(CONSTANTS.HIGHLIGHT);
+      const parentNode = getParent(cy, evtTarget);
+      highlightedNodeId.value.push(parentId);
+      parentNode.removeClass(CONSTANTS.NOT_SELECTED);
+      parentNode.addClass(CONSTANTS.HIGHLIGHT);
     }
   });
 
@@ -243,18 +272,38 @@ onMounted(() => {
     evtTarget.removeClass(CONSTANTS.MOUSE_HOVER);
   });
 
-  cy.dblclick(200);
+  // dbl click 간격?
+  cy.dblclick(100);
+  cy.on("dblclick", (evt: any) => {
+    const el = evt.target;
+    /**
+     * 상위 노드 dbl click 했을때, 상위 노드를 없애고 하위노드를 전개한다.
+     * 1. 하위노드들 edge 생성
+     * 2. 하위노드들 부모 삭제
+     * 3. 상위노드 삭제
+     * 4. filter 처리 추가
+     *  4-1. filter 를 parent 기준이 아니라, prnt_ctgry_id 를 기준으로 해야함.
+     *  parent 는 dbl click 으로 끊기기 때문에, 맨 처음에 노드를 한바퀴 돌면서 부모 정보를 노드 data에 저장해둠
+     *  그거를 가지고 처리 필요함.
+     */
 
-  cy.on("dblclick", (event: any) => {
-    console.log(event.target);
+    if (el.isParent()) {
+      el.children().style("background-color", el.style("background-color"));
+
+      const newEdges = createNewEdge(el.children(), getTarget(el));
+      cy.add(newEdges);
+      el.children().move({ parent: null });
+      // el.remove();
+      el.hide();
+    }
   });
 });
 
+const getColorCode = () => {
+  return Math.floor(Math.random() * 127 + 115).toString(16);
+};
 const getRandomColor = () => {
-  let color_r = Math.floor(Math.random() * 127 + 115).toString(16);
-  let color_g = Math.floor(Math.random() * 127 + 115).toString(16);
-  let color_b = Math.floor(Math.random() * 127 + 115).toString(16);
-  return `#${color_r + color_g + color_b}`;
+  return `#${getColorCode()}${getColorCode()}${getColorCode()}`;
 };
 
 const setNodeView = (id: string, isUnChecked: boolean) => {
--- frontend/composables/cytoscape/dragOpt.ts
+++ frontend/composables/cytoscape/dragOpt.ts
@@ -1,12 +1,60 @@
 import { ref } from "vue";
+import CONSTANTS from "@cons/constants";
 
 export default function useCytoscapeDragOpt() {
+  const getTarget = (targetNode: any) => {
+    return targetNode
+      .connectedEdges()
+      .map((e: any) =>
+        targetNode.data().id === e.data().source ? e.data().target : ""
+      );
+  };
+  const createNewEdge = (children: any[], targets: any) => {
+    let newEdges: any[] = [];
+    children.forEach((e) => {
+      newEdges = newEdges.concat(
+        targets.map((t: any, ti: number) => {
+          return {
+            group: "edges",
+            data: {
+              id: `${e.data().id}_${t}_${ti}`,
+              source: e.data().id,
+              target: t
+            }
+          };
+        })
+      );
+    });
+    return newEdges;
+  };
+
+  const findEls = (cy: any, key: string) => {
+    const arr: any[] = [];
+    cy.elements().forEach((el: any) => {
+      if (el.data(CONSTANTS.PRNT_CTGRY_ID) === key) {
+        arr.push(el);
+      }
+    });
+    return arr;
+  };
+  const getChild = (cy: any, el: any) => {
+    return findEls(cy, el.data().id);
+  };
+
+  const getSibling = (cy: any, el: any) => {
+    return findEls(cy, el.data(CONSTANTS.PRNT_CTGRY_ID));
+  };
+
+  const getParent = (cy: any, el: any) => {
+    return cy.getElementById(el.data(CONSTANTS.PRNT_CTGRY_ID));
+  };
+
   const options = ref({
-    grabbedNode: (node) => {
+    grabbedNode: (node: any) => {
       // 노드를 선택할 때 마다, 이벤트를 생성해주고 있기 때문에, 노드 선택시 이전 이벤트를 지우고 시작한다. (이벤트가 복수개로 설정되버림.)
       node.off("cdndout");
 
-      node.on("cdndout", (event, parentNode, dropSibling) => {
+      node.on("cdndout", (event: any, parentNode: any, dropSibling: any) => {
         const cy = event.cy;
         const thisNode = event.target;
 
@@ -20,11 +68,7 @@ export default function useCytoscapeDragOpt() {
          */
 
         // step1. 부모 노드의 edges 정보를 저장해둔다.
-        let targets = parentNode
-          .connectedEdges()
-          .map((e) =>
-            parentNode.data().id === e.data().source ? e.data().target : ""
-          );
+        let targets = getTarget(parentNode);
 
         // edges 정보를 물려줄 node 정보를 array 에 저장해두는 변수 선언
         // 선택한 노드는 dragout 하는 노드이기 때문에, children 에 넣어둔다.
@@ -48,39 +92,24 @@ export default function useCytoscapeDragOpt() {
         }
 
         // children 변수에는 부모노드의 edges 정보를 물려받아야 하는 노드들이 들어있다. 반영해준다.
-        let newEdges = [];
-        children.forEach((e) => {
-          newEdges = newEdges.concat(
-            targets.map((t, ti) => {
-              return {
-                group: "edges",
-                data: {
-                  id: `${e.data().id}_${t}_${ti}`,
-                  source: e.data().id,
-                  target: t
-                }
-              };
-            })
-          );
-        });
-        cy.add(newEdges);
+        cy.add(createNewEdge(children, targets));
       });
 
       return true;
     },
-    dropTarget: (dropTarget, grabbedNode) => {
+    dropTarget: (dropTarget: any, grabbedNode: any) => {
       // grabbedNode : 내가 선택한 노드
       // dropTarget : 내가 drop 할수 있는 모든 노드.
       // ->  분류를 추가하는 기능은 지원하지 않음 === 어떤 노드에도 drop 할 수 없음. 위치 이동만 가능함.
       return false;
     },
-    dropSibling: (dropSibling, grabbedNode) => {
+    dropSibling: (dropSibling: any, grabbedNode: any) => {
       // grabbedNode : 내가 선택한 노드
       // dropSibling : 고아노드를 제외한 내가 drop 할 수 있는 노드
       // dropTarget 은 false 고 dropSibling 은 true 일 수 없음.
       return false;
     },
-    newParentNode: (grabbedNode, dropSibling) => {
+    newParentNode: (grabbedNode: any, dropSibling: any) => {
       // 고아노드 + 고아노드 를 합칠때, 부모노드가 생성될때 호출 됨.
       // 노드 + 노드 를 설정할 수 없도록 했기 때문에 이 option method 에서는 아무것도 할 필요가 없음.
       return {};
@@ -93,6 +122,11 @@ export default function useCytoscapeDragOpt() {
     outThreshold: 200
   });
   return {
-    option: options
+    option: options,
+    getTarget: getTarget,
+    createNewEdge: createNewEdge,
+    getChild: getChild,
+    getParent: getParent,
+    getSibling: getSibling
   };
 }
